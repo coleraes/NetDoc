30fee4e02452cf5a4562eca140c4382e90ae9c66
Valentin <55053645+coleraes@users.noreply.github.com>
2021-10-28 19:47:34 +0300

    add RestController

--- NetDoc/src/main/java/com/NetDoc/controller/TestController.java
+++ NetDoc/src/main/java/com/NetDoc/controller/TestController.java
@@ -5,13 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "test";
+    public String greeting() {
+        return "Hello, world!";
     }
 
 }
