5b8bcd5a6e4e46cbd4897d41444223db0c62ba20
coleraes <timoshkin.vv@phystech.edu>
2021-11-02 18:23:02 +0300

    Fix for test controller

--- src/main/java/com/NetDoc/controller/TestController.java
+++ src/main/java/com/NetDoc/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.NetDoc.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import com.NetDoc.entity.TestEntity;
+import com.NetDoc.repos.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
+    @Autowired
+    private TestRepository testRepository;
 
     @GetMapping("/test")
-    public String greeting() {
-        return "Hello, world!";
+    public List<TestEntity> getTestEntities() {
+        return testRepository.findAll();
     }
 
 }
--- src/main/java/com/NetDoc/repos/TestRepository.java
+++ src/main/java/com/NetDoc/repos/TestRepository.java
@@ -3,10 +3,21 @@ package com.NetDoc.repos;
 import com.NetDoc.entity.TestEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface TestRepository extends CrudRepository<TestEntity, Long> {
 
-    List<TestEntity> findByText(String text);
+    default List<TestEntity> findAll() {
+        ArrayList<TestEntity> testEntities = new ArrayList<>();
+
+        testEntities.add(new TestEntity(1L, "Bratislava"));
+        testEntities.add(new TestEntity(2L, "Budapest"));
+        testEntities.add(new TestEntity(3L, "Prague"));
+        testEntities.add(new TestEntity(4L, "Warsaw"));
+        testEntities.add(new TestEntity(5L, "Los Angeles"));
+
+        return testEntities;
+    }
 
 }
