c14473423e207d6f32352f7aee98429ba5527df1
coleraes <timoshkin.vv@phystech.edu>
2021-11-05 19:43:33 +0300

    Add Upload and Download files

--- src/main/java/com/NetDoc/NetDocApplication.java
+++ src/main/java/com/NetDoc/NetDocApplication.java
@@ -1,9 +1,12 @@
 package com.NetDoc;
 
+import com.NetDoc.properties.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class NetDocApplication {
 
 	public static void main(String[] args) {
