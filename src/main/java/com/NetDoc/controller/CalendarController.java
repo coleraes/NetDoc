d6f5da6cea03871eb2caaa9290f88b68c7edbeea
lonay01 <shustov.sa@phystech.edu>
2021-11-18 19:15:57 +0300

    Revert

--- src/main/java/com/NetDoc/controller/CalendarController.java
+++
@@ -1,6 +0,0 @@
-package com.NetDoc.controller;
-
-
-public class CalendarController {
-
-}
--- src/main/java/com/NetDoc/repos/CalendarDataRepos.java
+++
@@ -1,8 +0,0 @@
-package com.NetDoc.repos
-
-import org.springframework.data.repository.CrudRepository;
-import com.NetDoc.DataBase.CalendarData;
-
-public interface CalendarDataRepos extends CrudRepository<CalendarData, Long> {
-
-}
